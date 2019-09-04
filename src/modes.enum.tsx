4e42932b92e4cf795e7a3d257964205cec685bd9
mwanago <wanago.marcin@gmail.com>
2019-09-05 00:34:31 +0200

    feat(server): listen for upcoming files

---
+++ src/modes.enum.tsx
@@ -0,0 +1,9 @@
+enum Modes {
+  ctr = 'ctr',
+  cbc = 'cbc',
+  cfb = 'cfb',
+  ofb = 'ofb',
+  ecb = 'ecb',
+}
+
+export default Modes;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import * as express from 'express';
+import * as multer from 'multer';
 
 const app = express();
+const upload = multer();
 
-app.post('/', (request, response) => {
-  response.send('Hello world!');
+app.post('/', upload.single('file'), (request, response) => {
+  const file = request.file;
+  const mode = request.body.mode;
+
+  console.log(mode, Boolean(file));
+  response.end();
 });
 
 app.listen(5000, () => {
