faf31d5e01f346c967f00d2cf998f6409ccaa7e1
mwanago <wanago.marcin@gmail.com>
2019-09-06 20:02:38 +0200

    feat(RSA): respond with public RSA key

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,24 @@
 import * as cors from 'cors';
 import 'dotenv/config';
 import * as express from 'express';
+import * as fs from 'fs';
 import * as multer from 'multer';
+import * as path from 'path';
 import Decryptor from './decryptor';
 import generateRSA from './generateRSA';
 
-generateRSA();
+const generatingPromise = generateRSA();
 
 const app = express();
 const upload = multer();
 
 app.use(cors());
+
+app.get('', async (request, response) => {
+  await generatingPromise;
+  const readStream = fs.createReadStream(path.resolve(__dirname, '../publicKeys/public.pem'));
+  readStream.pipe(response);
+});
 
 app.post('/', upload.single('file'), async (request, response) => {
   const file = request.file;
